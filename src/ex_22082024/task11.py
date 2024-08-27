06fe585729ef27dcc7410966a5db967aec2458f6
sandhya <shekhawat.sandhya232@gmail.com>
2024-08-27 08:33:43 +0530

    new tasks added

--- src/ex_22082024/task11.py
+++ src/ex_22082024/task11.py
@@ -1,14 +1,23 @@
-#fabonacci series
+# fabonacci series
 # 0 1 1 2 3 5 8
-#0 0+1 1+1 2+1 3+2 5+3
+# 0 0+1 1+1 2+1 3+2 5+3
 num_terms = int(input("Enter the number of terms in the Fibonacci series: "))
-a,b=0,1
+a, b = 0, 1
 
 print("fabonacci series")
-count=0
-while count<=num_terms:
-    print(a, end= ' ')
+count = 0
+while count <= num_terms:
+    print(a, end=' ')
 
-    a,b=b,a+b
-    count=count+1
+    a, b = b, a + b
+    count = count + 1
 
+"""
+   a,b=0,1
+   a,b=1,1
+   a,b=1,2
+   a,b=2,3
+   a,b=3,5
+   a,b=5,8
+   a,b=8,13
+"""
